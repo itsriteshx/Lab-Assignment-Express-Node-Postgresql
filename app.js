c16e23054b47140f72c4c9f0c3562fc50b1bae12
Ritesh <riteshroyal834047@gmail.com>
2026-09-20 23:06:05 +0530

    Implement DELETE assignment route

--- app.js
+++ app.js
@@ -67,6 +67,33 @@ app.patch("/assignments/:id",async(req,res)=>{
 
     }
 })
+
+
+app.delete("/assignments/:id",async(req,res)=>{
+    try{
+          const {id} =req.params;
+    const result=await pool.query(
+        "DELETE FROM assignments WHERE id=$1 RETURNING *",[id]
+    )
+    if (result.rows.length==0){
+        return res.status(400).json({
+            message: "Assignment not found"
+        })
+    }
+    res.json({
+        message: "Assignment deleted successfully",
+            assignment: result.rows[0]
+    })
+
+    }catch(err){
+         console.error(err);
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+  
+
+})
 app.listen(3000,(req,res)=>{
     console.log("Server is Successfully Running on port 3000")
 })
