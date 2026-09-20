79b173c2f631ea4e7b70401be4ff15edc5896f60
Ritesh <riteshroyal834047@gmail.com>
2026-09-20 22:59:39 +0530

    Implement PATCH assignment route

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.get("/assignments", async (req, res) => {
     }
 });
 
+
+
 app.post("/assignments",async(req,res)=>{
     try{
         const {title,deadline}=req.body;
@@ -41,6 +43,27 @@ app.post("/assignments",async(req,res)=>{
         res.status(500).json({
             message: "Server error"
         });
+
+    }
+})
+
+
+app.patch("/assignments/:id",async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const result=await pool.query(
+            "UPDATE assignments SET submitted=true WHERE id=$1 RETURNING *",[id]);
+        if (result.rows.length==0){
+            return res.status(404).json({
+                 message: "Assignment not found"
+            })    
+        }    
+        res.status(200).json(result.rows[0])
+    }catch(err){
+        console.error(err)
+        res.status(500).json({
+            message: "Server error"
+        })
 
     }
 })
